0536290d1d55ff5422839e3db8b09df4d2f97169
Bha <bha@Jhsync254.afuya.co>
2024-10-24 18:18:44 +0300

    more aesthetics

--- config.py
+++ config.py
@@ -7,12 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    # Secret key for security
-    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')
-
-    # Database configuration
-    SQLALCHEMY_DATABASE_URI ='sqlite:///sale.db' #'postgresql://postgres:WakpFCEZjrKOmisavwiSjjbVLSuqqjQB@junction.proxy.rlwy.net:40841/railway'  # Replace this with os.getenv for flexibility if needed
+    SECRET_KEY = os.getenv('SECRET_KEY', 'you-will-never-guess')  
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Flask-SocketIO configurations (removed Redis-related message queue)
     SOCKETIO_MESSAGE_QUEUE = None
